431e9b4ce3feec441b4b870ddce0885ff60121f5
Sindhu Inti <iamsindhuinti23@gmail.com>
2023-01-13 19:10:17 +0530

    Buy and sell function added

---
+++ src/opend_assets/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import homeImage from "../../assets/home-img.png";
+import Item from "./Item";
+import Minter from "./Minter";
+
+function App() {
+
+  // const NFTID= "rrkah-fqaaa-aaaaa-aaaaq-cai";
+  return (
+    <div className="App">
+      <Header />
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
